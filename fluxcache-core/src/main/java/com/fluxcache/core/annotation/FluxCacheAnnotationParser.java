7add70e1f4232cebd5574d6ec7502f5b99f9893f
weihu <weihu@apache.org>
2024-09-11 22:36:41 +0800

    add annotation

--- fluxcache-core/src/main/java/com/fluxcache/core/annotation/FluxCacheAnnotationParser.java
+++ fluxcache-core/src/main/java/com/fluxcache/core/annotation/FluxCacheAnnotationParser.java
@@ -1,9 +1,23 @@
 package com.fluxcache.core.annotation;
 
+import com.fluxcache.core.model.FluxCacheOperation;
+import java.lang.reflect.Method;
+import org.springframework.lang.Nullable;
+
 /**
  * @author : wh
  * @date : 2024/9/11 22:28
  * @description:
  */
-public class FluxCacheAnnotationParser {
+public interface FluxCacheAnnotationParser {
+
+    default boolean isCandidateClass(Class<?> targetClass) {
+        return true;
+    }
+
+    @Nullable
+    FluxCacheOperation parseCacheAnnotation(Class<?> type);
+
+    @Nullable
+    FluxCacheOperation parseCacheAnnotation(Method method);
 }
