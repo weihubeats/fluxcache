a512e02df1764bc3189340dc147e088972b9c542
weihu <weihu@apache.org>
2024-11-06 22:51:47 +0800

    init

--- fluxcache-admin/src/main/java/com/fluxcache/admin/controller/FluxCacheController.java
+++ fluxcache-admin/src/main/java/com/fluxcache/admin/controller/FluxCacheController.java
@@ -1,9 +1,23 @@
 package com.fluxcache.admin.controller;
 
+import com.fluxcache.core.FluxCacheManager;
+import com.fluxcache.core.properties.FluxCacheProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author : wh
  * @date : 2024/8/31 15:12
  * @description:
  */
+@RestController
+@RequestMapping("${flux.cache.prefix:/cache/manager/v1}")
+@RequiredArgsConstructor
 public class FluxCacheController {
+
+    private final FluxCacheManager cacheManager;
+
+    private final FluxCacheProperties cacheProperties;
+
 }
