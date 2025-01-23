de6e7555b24d35112a9cdaa91d42f85a4409cfc3
weihu <weihu@apache.org>
2025-01-23 10:01:27 +0800

    add docker test

--- fluxcache-example/src/test/java/com/fluxcache/example/controller/TestControllerTest.java
+++ fluxcache-example/src/test/java/com/fluxcache/example/controller/TestControllerTest.java
@@ -15,7 +15,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -29,7 +35,21 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @Slf4j
+@Testcontainers
 class TestControllerTest {
+    
+    @Container
+    public static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:6.2.6"))
+        .withExposedPorts(6379);
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        System.out.println(redis.getHost());
+        System.out.println(redis.getMappedPort(6379).toString());
+        registry.add("redis.host", redis::getHost);
+        registry.add("redis.port", () -> redis.getMappedPort(6379).toString());
+        registry.add("redis.password", () -> "");
+    }
 
     private final static Long SLEEP_TIME = 3L;
 
